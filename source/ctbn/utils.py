6de2dd7c1e99ca47bee5d03df41ffa2019c7e950
gekinci <gizemekinnci@gmail.com>
2019-12-03 17:28:32 +0100

    amalgamation function from ctbn paper to get state transition funtion from parents CIMs

--- source/ctbn/utils.py
+++ source/ctbn/utils.py
@@ -11,6 +11,7 @@ def cartesian_products(n):
 def zero_div(x, y):
     return x / y if y != 0 else 0
 
+
 #
 # def merge_independent_ctbn_trajectories(df1, df2):
 #     df_joined = pd.concat([df1, df2], ignore_index=True, sort=False).sort_values(
@@ -23,3 +24,15 @@ def random_q_matrix(n_values):
     np.fill_diagonal(tmp, 0)
     Q = tmp.tolist()
     return Q
+
+
+def amalgamation_independent_cim(Q1, Q2):
+    x1 = Q1[0][1]
+    y1 = Q1[1][0]
+    x2 = Q2[0][1]
+    y2 = Q2[1][0]
+    T = np.repeat(np.array([[(x1 + x2) / 2, x2, x1, .0],
+                            [y2, (y2 + x1) / 2, .0, x1],
+                            [y1, .0, (y1 + x2) / 2, x2],
+                            [.0, y1, y2, (y1 + y2) / 2]])[np.newaxis, :, :], 3, axis=0)
+    return T
