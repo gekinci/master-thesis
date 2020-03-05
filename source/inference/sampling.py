21958a47089f022bea9a678e2bdaa992b077d738
gekinci <gizemekinnci@gmail.com>
2020-03-05 02:04:26 +0100

    likelihood of inhomogenous CTMC

---
+++ source/inference/sampling.py
@@ -0,0 +1,32 @@
+import numpy as np
+import itertools
+from utils.constants import *
+from utils.helpers import *
+
+
+def sample_obs_model(n_states, n_obs):
+    z = np.zeros((n_states, n_obs))
+    z[np.arange(n_states), np.random.randint(0, n_obs, size=(n_states))] = 1
+    return z
+
+
+def obs_model_set(n_states, n_obs):
+    return np.array(list(itertools.product(np.eye(n_obs), repeat=n_states)))
+
+
+def log_likelihood_inhomogeneous_ctmc(df_traj, df_Q, node='Z'):
+    L = 0
+    df_trans = df_traj.loc[df_traj[node].diff() != 0]
+    trans_tuples = list(
+        zip(df_trans[node].astype(int), df_trans[node][1:].astype(int), df_trans[TIME], df_trans[TIME][1:]))
+    for i in trans_tuples:
+        trans_tag = ''.join(map(str, i[0:2]))
+        stay_tag = ''.join((str(i[0]), str(i[0])))
+        times = i[2:4]
+        q_trans = df_Q.loc[to_decimal(times[1]), trans_tag]
+        df_Q_ = df_Q.truncate(before=to_decimal(times[0]), after=to_decimal(times[1]))
+        prob_stay = (df_Q_[stay_tag].multiply(df_Q_[T_DELTA], axis="index")).cumsum().loc[to_decimal(times[1])]
+        prob_trans = df_Q.loc[to_decimal(times[1]), trans_tag] / abs(df_Q.loc[to_decimal(times[1]), stay_tag])
+        print(np.log(q_trans) + np.log(prob_trans) + prob_stay)
+        L += np.log(q_trans) + np.log(prob_trans) + prob_stay
+    return L
